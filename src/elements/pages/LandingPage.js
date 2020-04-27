c1e55a3e6c0b356281ad7e3fd4eb5f98eaad3a63
Muhammad Abi Muzaki <mhd.abim24@gmail.com>
2020-04-27 17:26:40 +0700

    create header component

---
+++ src/elements/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import Header from "elements/parts/Header";
+
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <>
+        <Header {...this.props} />
+      </>
+    );
+  }
+}
---
+++ src/elements/parts/IconText.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Button from "elements/Button";
+export default function IconText() {
+  return (
+    <Button className="brand-text-icon" href="wew" type="link">
+      Stay<span class="text-gray-900">cation.</span>
+    </Button>
+  );
+}
